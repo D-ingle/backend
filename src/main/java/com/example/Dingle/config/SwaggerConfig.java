c7b3435ed97ba24789e4b481218c2fc34d7959a7
jeongIN <shjin4667@gmail.com>
2026-01-20 16:34:34 +0900

    swagger api

--- src/main/java/com/example/Dingle/config/SwaggerConfig.java
+++ src/main/java/com/example/Dingle/config/SwaggerConfig.java
@@ -1,4 +1,25 @@
 package com.example.Dingle.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(info());
+    }
+
+    private Info info() {
+        return new Info()
+                .title("Dingle Swagger API")
+                .version("v1.0.0")
+                .description("Dingle API");
+    }
 }
