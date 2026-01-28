d9ab2e1f6a3a5f503a5791734cb0669f2fd2c00a
HUIJAEKO <xrhgmlwox@naver.com>
2026-01-28 17:02:40 +0900

    fix: api endpoint 수정

--- src/main/java/com/example/Dingle/user/controller/RegisterController.java
+++ src/main/java/com/example/Dingle/user/controller/RegisterController.java
@@ -7,12 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RequestMapping("/api/v1")
+@RestController
 @ResponseBody
 public class RegisterController {
 
