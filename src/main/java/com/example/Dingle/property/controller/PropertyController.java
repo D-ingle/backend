40f23a374785def6e0fd477f412099111f2d1fb8
jeongIN <shjin4667@gmail.com>
2026-02-03 23:06:02 +0900

    feat: recent property

--- src/main/java/com/example/Dingle/property/controller/PropertyController.java
+++ src/main/java/com/example/Dingle/property/controller/PropertyController.java
@@ -1,15 +1,16 @@
 package com.example.Dingle.property.controller;
 
 import com.example.Dingle.global.dto.ResponseDTO;
+import com.example.Dingle.property.dto.PropertyListDTO;
 import com.example.Dingle.property.dto.PropertyRegisterRequestDTO;
+import com.example.Dingle.property.service.PropertyListService;
 import com.example.Dingle.property.service.PropertyService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PropertyController {
 
     private final PropertyService propertyService;
+    private final PropertyListService propertyListService;
 
     @PostMapping("/register")
     @Operation(summary = "매물 등록 API", description = "매물을 등록합니다.")
@@ -25,5 +27,12 @@ public class PropertyController {
     ) {
         propertyService.register(request);
         return ResponseEntity.ok(ResponseDTO.success(null));
+    }
+
+    @GetMapping("/recent")
+    public ResponseEntity<ResponseDTO<List<PropertyListDTO>>> recentView(@RequestParam("propertyIds") List<Long> propertyIds) {
+
+        List<PropertyListDTO> response = propertyListService.getPropertyList(propertyIds);
+        return ResponseEntity.ok(ResponseDTO.success(response));
     }
 }
