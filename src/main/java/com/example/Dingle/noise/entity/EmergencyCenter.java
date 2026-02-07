8e9a09fb61efda39c956f06de634a7f3b56592e6
jeongIN <shjin4667@gmail.com>
2026-02-07 19:06:15 +0900

    feat: entity geom 컬럼 추가

--- src/main/java/com/example/Dingle/noise/entity/EmergencyCenter.java
+++ src/main/java/com/example/Dingle/noise/entity/EmergencyCenter.java
@@ -3,6 +3,7 @@ package com.example.Dingle.noise.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
 
 @Entity
 @Getter
@@ -20,6 +21,9 @@ public class EmergencyCenter {
     private Double latitude;
     @Column(nullable = false)
     private Double longitude;
+
+    @Column(columnDefinition = "geometry(Point,4326)")
+    private Point geom;
 
     public EmergencyCenter(String name, Double latitude, Double longitude) {
         this.name = name;
